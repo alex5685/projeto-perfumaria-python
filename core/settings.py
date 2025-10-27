8ab188d269300368aa6eee63830f936a810a5c6f
alex5685 <alex5685@gmail.com>
2025-10-27 13:24:32 -0300

    FIX: Removido AWS_DEFAULT_ACL para respeitar ACLs desabilitadas do S3.

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ if AWS_STORAGE_BUCKET_NAME:
     
     # Configurações de acesso e segurança
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read' # Permite que as fotos sejam visíveis
+   # AWS_DEFAULT_ACL = 'public-read' # Permite que as fotos sejam visíveis
     
     # Monta o domínio completo para que o Django use para servir as fotos
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'

e576ae92095f8cead61a5f1027b58d3f7f974a54
alex5685 <alex5685@gmail.com>
2025-11-03 23:07:38 -0300

    loja:2 corrigindo classe pedidos

--- loja/admin.py
+++ loja/admin.py
@@ -1,19 +1,47 @@
 from django.contrib import admin
-from .models import Produto, Pedido
+from .models import Produto, Pedido, LogWhatsapps
+
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("id", "nome", "preco_venda", "quantidade_estoque", "disponivel_no_site", "criado_em")
-    list_filter = ("disponivel_no_site",)
-    search_fields = ("nome", "descricao_detalhada")
+    # Campos que EXISTEM em Produto (conforme seu models/migrações atuais)
+    list_display = (
+        "id",
+        "nome",
+        "preco_venda",
+        "quantidade_estoque",
+        "disponivel_no_site",
+        "criado_em",
+        "atualizado_em",
+    )
+    list_filter = ("disponivel_no_site", "criado_em")
+    search_fields = ("nome",)
+    ordering = ("-criado_em",)
+    date_hierarchy = "criado_em"
     readonly_fields = ("criado_em", "atualizado_em")
+
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    # Deixe BEM simples até validarmos os campos existentes:
-    list_display = ("id", "criado_em", "atualizado_em")
-    readonly_fields = ("criado_em", "atualizado_em")
+    """
+    ATENÇÃO: Deixe apenas campos que existem no modelo Pedido.
+    Pelo histórico, Pedido tem 'id' e 'criado_em'. 'atualizado_em' não existe nele.
+    Se, futuramente, você adicionar mais campos ao Pedido, pode incluir aqui.
+    """
+    list_display = ("id", "criado_em")
+    ordering = ("-criado_em",)
+    date_hierarchy = "criado_em"
+    readonly_fields = ("criado_em",)
 
-    # Remova QUALQUER coisa que referencie campos que não existem mais:
-    # nada de ordering, list_filter, date_hierarchy, search_fields com
-    # campos antigos (status, cliente, total, etc.).
+
+@admin.register(LogWhatsapps)
+class LogWhatsappsAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "mensagem_preview")
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    readonly_fields = ("created_at",)
+
+    def mensagem_preview(self, obj):
+        # evita log gigante na listagem
+        return (obj.mensagem or "")[:80]
+    mensagem_preview.short_description = "Mensagem"
